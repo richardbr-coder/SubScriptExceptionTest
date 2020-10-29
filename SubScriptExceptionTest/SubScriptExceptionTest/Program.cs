07e92583033d3d02b4aa75c57c5cce1e28024723
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:14:43 -0230

    Add files via upload

---
+++ SubScriptExceptionTest/SubScriptExceptionTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubScriptExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[] { 
+                123, 
+                234, 
+                345,
+                456,
+                567,
+                678,
+                789,
+                8910,
+                91011,
+                101112 
+            };
+            while (true)
+            {
+                try
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Write("\nEnter a subscript possition between 0 & 9 to see the value stored there: ");
+                        int user = Convert.ToInt32(ReadLine());
+
+                        Write("The value stored in that posistion is: " + arr[user] + "\n");
+                    }
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    WriteLine("*ERROR " + e.Message + " Please try again.");
+
+                }
+            }
+            
+        }
+    }
+}
